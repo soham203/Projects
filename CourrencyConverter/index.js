b3f9770a8630b7bb8a10e989a22092048521d5a2
soham203 <soham203patil@gmail.com>
2024-04-25 01:03:17 +0530

    Projrct completed

--- CourrencyConverter/index.js
+++ CourrencyConverter/index.js
@@ -1,7 +1,58 @@
-const baseLink = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const baseLink = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropDowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-for (code in countryList) {
-    console.log(code);
-  } 
+for (let select of dropDowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerHTML = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+      } 
+
+      select.addEventListener("change", (evt) => {
+            updateFlag(evt.target);
+      });
+}
+
+const updateExangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    const URL = `${baseLink}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
+
+const updateFlag = (element) => {
+   let currCode = element.value;
+   let countryCode = countryList[currCode];
+   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+   let img = element.parentElement.querySelector("img");
+   img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExangeRate();
+});
