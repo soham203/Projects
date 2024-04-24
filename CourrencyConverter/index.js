2116f5a912affea0b8dae0c5952bf315a16d606b
soham203 <soham203patil@gmail.com>
2024-04-24 19:39:46 +0530

    first push

---
+++ CourrencyConverter/index.js
@@ -0,0 +1,7 @@
+const baseLink = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+const dropDowns = document.querySelectorAll(".dropdown select");
+
+for (code in countryList) {
+    console.log(code);
+  } 
